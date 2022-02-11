f73f9146d117dd5c82c5a002b7b6480cb884cec3
Kyle Chiu <kylechiu.kc@gmail.com>
2022-02-12 02:09:16 +0800

    feat: Add server setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,16 @@
-async function main() {}
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { ApolloServer } from "apollo-server";
+import { buildSchema } from "type-graphql";
+import { resolvers } from "./resolvers";
+
+async function main() {
+  await createConnection();
+  const schema = await buildSchema({ resolvers });
+  const server = new ApolloServer({ schema });
+  await server.listen(4000);
+}
+
+main().catch((err) => {
+  console.error(err);
+});
